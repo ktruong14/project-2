61f6037b7b73cc09b366f3486e1c2c686da7ec45
Shona Critch <shona.critch@gmail.com>
2021-01-09 13:06:05 +1100

    updated app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 #################################################
@@ -36,7 +36,12 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+# create route that renders index.html template
 @app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/menu")
 def welcome():
     """List all available api routes."""
     return (
